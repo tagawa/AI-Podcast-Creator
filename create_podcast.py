372ed8701cc4e78f25bc9866bd1842fe255770d0
Daniel Davis <tagawa@users.noreply.github.com>
2023-03-02 18:56:28 +0900

    Added first version of script

---
+++ create_podcast.py
@@ -0,0 +1,72 @@
+import json
+from dotenv import load_dotenv
+import feedparser
+import openai
+import os
+import requests
+
+# Make sure you've added your API keys to the .env environment variables file.
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+elevenlabs_api_key = os.getenv("ELEVENLABS_API_KEY")
+
+# Uncomment or add your preferred feed here.
+#news_feed = "https://feeds.npr.org/1001/rss.xml" # NPR News
+#news_feed = "http://feeds.bbci.co.uk/news/rss.xml?edition=int" # BBC News
+news_feed = "https://www.japantimes.co.jp/news_category/national/feed/" # Japan Times News
+
+
+print("## Getting RSS feed content…")
+
+feed = feedparser.parse(news_feed)
+stories = ""
+stories_limit = 10 # The maximum number of news stories to include.
+
+# Loop through each item in the RSS feed and append each title and description to the "stories" string.
+for item in feed.entries[:stories_limit]:
+  stories = stories + " News Story: " + item.title + ". " + item.description
+
+
+print("## Generating ChatGPT output…")
+
+# See also OpenAI's ChatGPT API guide: https://platform.openai.com/docs/guides/chat
+chat_output = openai.ChatCompletion.create(
+  model = "gpt-3.5-turbo",
+  messages = [{
+    "role": "user",
+    "content": "Please rewrite the following news headlines and summaries in a discussion way, as though someone is talking about them one by one on a one-off podcast in a non-judgemental way and with no follow-on discussion, although there should be a final closing greeting: " + stories
+  }]
+)
+chat_content = chat_output.choices[0].message.content
+
+print(chat_content)
+
+
+print("## Generating audio output…")
+
+# See also the ElevenLabs API docs: https://api.elevenlabs.io/docs
+voice_id = "21m00Tcm4TlvDq8ikWAM" # The voice ID for "Rachel"
+audio_output = requests.post(
+  "https://api.elevenlabs.io/v1/text-to-speech/" + voice_id,
+  data = json.dumps({
+    "text": chat_content,
+    "voice_settings": {
+      "stability": 0.2, # The lower the number, the more expressive the voice.
+      "similarity_boost": 0
+    }
+  }),
+  headers = {
+    "Content-Type": "application/json",
+    "xi-api-key": elevenlabs_api_key,
+    "accept": "audio/mpeg"
+  },
+)
+
+# Check for a valid audio output, otherwise print the error message.
+if audio_output.status_code == 200:
+  with open('test.mp3', 'wb') as output_file:
+    output_file.write(audio_output.content)
+else:
+  print(audio_output.text)
+
+print("## All processing complete.")
